1318ef0915212ffc918e04b6a304d69751f3cf7b
김도형 <dohyung97022@gmail.com>
2022-04-15 20:05:12 +0900

    [#1] Adb 내의 기기의 uuid 반환.

--- appium/domain/common/Platform_type.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-# 기기의 OS
-class PlatformName(enum.Enum):
-    Android: str = 'Android'
-    IOS: str = 'IOS'
---
+++ sub_process/domain/adb/adb_commands.py
@@ -0,0 +1,3 @@
+class AdbCommands:
+    # adb 에 연결된 기기의 id 와 연결 상태 반환
+    get_all_devices: list[str] = ['adb', 'devices']
